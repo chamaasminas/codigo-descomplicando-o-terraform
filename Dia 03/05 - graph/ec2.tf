d746c4c99cc37ac62fcab669c58011058a3a21d4
Isadoramenezes <isadoranezes@gmail.com>
2020-05-16 20:19:36 -0300

    Adição aula 5 - graph

---
+++ Dia 03/05 - graph/ec2.tf
@@ -0,0 +1,20 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["IaaSWeek-${var.hash_commit}"]
+  }
+
+  owners = ["777015859311"] # Gomex ID, não mude sem mudar o filtro
+}
+
+resource "aws_instance" "web" {
+  count         = 2
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id     = " " # <------ altere para sua subnet
+  tags = {
+    Name = "HelloWorld"
+  }
+}
