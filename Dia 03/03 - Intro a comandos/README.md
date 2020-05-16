4b8f1f1a13587adb1f8acacc8c975fa3f57abab4
Isadoramenezes <isadoranezes@gmail.com>
2020-05-16 20:04:27 -0300

    Adição aula 3

---
+++ Dia 03/03 - Intro a comandos/README.md
@@ -0,0 +1,4 @@
+# Considerações
+
+- Da mesma forma da primeira aula deste dia, não houve alteração no código nesta aula
+- O código nesse repo é o mínimo para poder rodar o plan e apply com o TF_LOG = DEBUG
---
+++ Dia 03/03 - Intro a comandos/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region  = "us-east-1"
+  version = "~> 2.0"
+}
+
+terraform {
+  backend "s3" {
+    bucket = " " # <------ ALTERE AQUI   
+    key            = "terraform-test.tfstate"
+    region         = "us-east-1"
+  }
+}
